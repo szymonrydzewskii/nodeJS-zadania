c4bc8b652ddfff37fbf8175d393a648ff25b254e
Szymon <s.rydzewski26@gmail.com>
2024-11-01 19:26:12 +0100

    Stworzenie funkcji obliczania metoda callback, ustawiono opoznienie na sekunde i stworzenie wypisywania wyniku

--- async-operations-calculator/kalkulator.js
+++ async-operations-calculator/kalkulator.js
@@ -3,9 +3,36 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
+
+function metoda_callback(a, b, operacja, callback){
+    setTimeout(() => {
+        var wynik = 0
+        if(operacja === "1"){
+            wynik = a + b
+        }
+        else if(operacja === "2"){
+            wynik = a * b
+        } else{
+            return callback("błąd")
+        }
+        callback(wynik)
+    }, 1000)
+}
+
 rl.question("podaj pierwszą liczbę: ", (input_a) => {
     rl.question("podaj drugą liczbę: ", (input_b) => {
         const a = parseInt(input_a)
         const b = parseInt(input_b)
+
+        rl.question("Wybierz operacje (dodawanie = 1, mnożenie = 2): ", (operacja) => {
+            metoda_callback(a, b, operacja, (error, wynik) => {
+                if (error){
+                    console.log(error)
+                } else{
+                    console.log("Wynik: ", wynik)
+                }
+                rl.close()
+            })
+        })
     })
 })
