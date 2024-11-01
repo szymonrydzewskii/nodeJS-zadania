ca59fc577c7f4f2e57f3635eca625ffbe86b72ee
Szymon <s.rydzewski26@gmail.com>
2024-11-01 21:57:56 +0100

    Stworzenie Watchera

--- file-watcher/file-watcher.js
+++ file-watcher/file-watcher.js
@@ -1,2 +1,30 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require("fs");
+const path = require("path");
+
+const sciezkaWatcher = './file-watcher';
+const logSciezka = path.join(sciezkaWatcher, "plikWatcher.log");
+function logEvent(tresc) {
+    const logTresc = `${new Date().toISOString()} - ${tresc}\n`;
+    fs.appendFileSync(logSciezka, logTresc);
+    console.log(logTresc.trim());
+}
+if (!fs.existsSync(logSciezka)) {
+    fs.writeFileSync(logSciezka, '');
+}
+fs.watch(sciezkaWatcher, (eventType, filename) => {
+    if (filename) {
+        if (filename === 'plikWatcher.log') {
+            return;
+        }
+
+        if (eventType === 'rename') {
+            lastEvent = `Dodano lub usunięto plik: ${filename}`;
+            logEvent(lastEvent)
+        } else if (eventType === 'change') {
+            lastEvent = `Zmiana w pliku: ${filename}`;
+            logEvent(lastEvent)
+        }
+    }
+});
+
+console.log(`Monitoruję zmiany w folderze: ${sciezkaWatcher}`);
