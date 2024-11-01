e4f4ce9a038ec4642117fb070f645a5f87dbe845
Szymon <s.rydzewski26@gmail.com>
2024-11-01 18:32:25 +0100

    Stworzenie interfejsu

--- data-logger/index.js
+++ data-logger/index.js
@@ -1,3 +1,7 @@
 import fs from "fs"
 import {promises as fsPromises} from "fs"
 const readline = require("readline")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
