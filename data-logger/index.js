080016b5f371c3f5209d7b61bf0c373d9e928cd7
Szymon <s.rydzewski26@gmail.com>
2024-11-01 18:52:15 +0100

    Troche zmiana w wygladzie kodu zeby bylo bardziej przejrzyscie

--- data-logger/index.js
+++ data-logger/index.js
@@ -1,18 +1,25 @@
 const fs = require("fs")
 const readline = require("readline")
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
+
 rl.question("podaj swoje imię: ", (name) => {
+
     rl.question("podaj swoje nazwisko: ", (surname) => {
+
         rl.question("podaj swój wiek: ", (age) => {
+
             const daneUser = {
                 name: name,
                 surname: surname,
                 age: age
             }
+
             fs.writeFileSync("dane.json", JSON.stringify(daneUser))
+            
             const dane = fs.readFileSync("dane.json", "utf8")
             console.log("Odczytane dane: ", JSON.parse(dane))
 
