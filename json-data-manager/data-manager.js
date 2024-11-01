d353c436bc62a315ec28b2672c41c59355ec79d7
Szymon <s.rydzewski26@gmail.com>
2024-11-01 21:06:37 +0100

    pobieranie danych od uzytkownika i stworzenie funkcji dodawania danych

--- json-data-manager/data-manager.js
+++ json-data-manager/data-manager.js
@@ -5,3 +5,29 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
+async function dodawanie(sciezka, nowyObiekt){
+    try{
+        const data = await JSON.parse(fs.promises.readFile(`json-data-manager/${sciezka}`, 'utf8'))
+        data.push(nowyObiekt)
+        await fs.promises.writeFile(`json-data-manager/${sciezka}`, JSON.stringify(data))
+        console.log("Dodano dane")
+    } catch(err){
+        await fs.promises.writeFile(`json-data-manager/${sciezka}`, JSON.stringify([nowyObiekt]))
+    }
+}
+rl.question("wpisz nazwe pliku z jakim chcesz pracować (np. dane.json): ", (sciezka) => {
+        rl.question("chcesz dodać nowy obiekt czy wyświetlić dane? (dodać = 1, wyświetlić dane = 2): ", (wybor) =>{
+        if(wybor === "1"){
+            rl.question("podaj imię: ", (name) => {
+                rl.question("podaj wiek: ", (age) => {
+                    rl.question("podaj email: ", (email) => {
+                        const nowyObiekt = {name, age: parseInt(age), email}
+                        dodawanie(sciezka, nowyObiekt)
+                        rl.close()
+                            })
+                        })
+                    })
+                }}
+            )
+        }
+    )
