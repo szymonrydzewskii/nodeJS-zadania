b1aba6cddafe962b97c5d6d45c172d5525463e3e
Szymon <s.rydzewski26@gmail.com>
2024-11-01 18:44:59 +0100

    Stworzenie zadawania pytan, zmiana (import fs) na (const fs) bo jakis blad wywalalo, przechowywanie danych

--- data-logger/index.js
+++ data-logger/index.js
@@ -1,7 +1,17 @@
-import fs from "fs"
-import {promises as fsPromises} from "fs"
+const fs = require("fs")
 const readline = require("readline")
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
+})
+rl.question("podaj swoje imię: ", (name) => {
+    rl.question("podaj swoje nazwisko: ", (surname) => {
+        rl.question("podaj swój wiek: ", (age) => {
+            const daneUser = {
+                name: name,
+                surname: surname,
+                age: age
+            }
+        })
+    })
 })
