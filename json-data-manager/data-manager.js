478b1a86821cc4ae46182f4941405ecc30c50929
Szymon <s.rydzewski26@gmail.com>
2024-11-01 21:29:59 +0100

    wyswietlanie danych z pliku

--- json-data-manager/data-manager.js
+++ json-data-manager/data-manager.js
@@ -7,14 +7,26 @@ const rl = readline.createInterface({
 })
 async function dodawanie(sciezka, nowyObiekt){
     try{
-        const data = await JSON.parse(fs.promises.readFile(`json-data-manager/${sciezka}`, 'utf8'))
-        data.push(nowyObiekt)
-        await fs.promises.writeFile(`json-data-manager/${sciezka}`, JSON.stringify(data))
+        const data = await fs.promises.readFile(`json-data-manager/${sciezka}`, 'utf8')
+        let parsedData = []
+        parsedData = JSON.parse(data)
+        parsedData.push(nowyObiekt)
+        await fs.writeFile(`json-data-manager/${sciezka}`, JSON.stringify(data, null, 2))
         console.log("Dodano dane")
     } catch(err){
         await fs.promises.writeFile(`json-data-manager/${sciezka}`, JSON.stringify([nowyObiekt]))
     }
 }
+
+function wyswietlanie(sciezka){
+    try{
+        const data = JSON.parse(fs.readFileSync(`json-data-manager/${sciezka}`, 'utf8'))
+        console.log(data)
+    } catch(err){
+        console.error(err)
+    }
+}
+
 rl.question("wpisz nazwe pliku z jakim chcesz pracować (np. dane.json): ", (sciezka) => {
         rl.question("chcesz dodać nowy obiekt czy wyświetlić dane? (dodać = 1, wyświetlić dane = 2): ", (wybor) =>{
         if(wybor === "1"){
@@ -27,7 +39,15 @@ rl.question("wpisz nazwe pliku z jakim chcesz pracować (np. dane.json): ", (sci
                             })
                         })
                     })
-                }}
+                }
+            else if(wybor === "2"){
+                wyswietlanie(sciezka)
+                rl.close()
+            } else{
+                console.log("błąd")
+                rl.close()
+            }
+        }
             )
         }
     )
