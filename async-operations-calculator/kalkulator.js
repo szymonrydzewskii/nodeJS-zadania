6c92d4e7d223f0d74b1bae37160a5912ce58a280
Szymon <s.rydzewski26@gmail.com>
2024-11-01 19:08:38 +0100

    Pobieranie liczb od uzytkownika

--- async-operations-calculator/kalkulator.js
+++ async-operations-calculator/kalkulator.js
@@ -3,3 +3,9 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
+rl.question("podaj pierwszą liczbę: ", (input_a) => {
+    rl.question("podaj drugą liczbę: ", (input_b) => {
+        const a = parseInt(input_a)
+        const b = parseInt(input_b)
+    })
+})
